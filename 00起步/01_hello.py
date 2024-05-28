4438bba2d7c22edc8b57d1087824786e6927aa7c
liyongjie <liyongjie@twyfordtile.com>
2024-05-28 10:41:13 +0800

    简单的变量和类型操作

---
+++ 00起步/01_hello.py
@@ -0,0 +1,30 @@
+# # 变量
+# name = "Tom"
+# print(name)
+# print(type(name))
+# name = 2
+# print(name)
+# print(type(name))
+
+# 字符串
+# # 格式化输出
+# name2 = "ada lovelace"
+# print(name2.title())
+# print(name2.upper())
+# print(name2.lower())
+
+# # 删除空格
+# str = ' kek  '
+# # print(str.strip())
+# # print(str.lstrip())
+# print(str.rstrip())
+
+# # 数字
+# print(3**3)
+# print(0.1 + 0.2)
+# # 类型错误：字符串和数字不能相加
+# # print("happy" + 23 + "rd birthday")
+# print("happy" + str(23) + "rd birthday")
+
+# # python 禅
+# import this
